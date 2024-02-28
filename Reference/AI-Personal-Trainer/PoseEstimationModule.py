2a3e0b4c95a46df27610d73466ec5182b9e13fa3
Sunil Kumar P <sunilkpg23@gmail.com>
2024-02-28 10:23:50 +0530

    10:29 update

--- Reference/AI-Personal-Trainer/PoseEstimationModule.py
+++ Reference/AI-Personal-Trainer/PoseEstimationModule.py
@@ -66,6 +66,7 @@ class poseDetector():
             cv2.circle(img, (x3, y3), 5, (255, 0, 0), 2)
             cv2.putText(img,str(int(angle)),(x2-20,y2+50),cv2.FONT_HERSHEY_PLAIN,2,(255,0,255),2)
         return angle
+    
 def main():
     cap = cv2.VideoCapture(0)
     pTime=0
